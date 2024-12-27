ea72582a01c4b1534a58f29f3207398433c4eb59
Edanriell <Edanriell@gmail.com>
2024-12-27 21:00:09 +0200

    feature(skills): created markup and class names

--- Client/src/next-pages/home/ui/home-page.tsx
+++ Client/src/next-pages/home/ui/home-page.tsx
@@ -1,7 +1,13 @@
 import { type FC } from "react";
 
 import { Hero } from "./sections/hero/ui";
+import { Skills } from "./sections/skills/ui";
 
 export const HomePage: FC = () => {
-	return <Hero />;
+	return (
+		<>
+			<Hero />
+			<Skills />
+		</>
+	);
 };
