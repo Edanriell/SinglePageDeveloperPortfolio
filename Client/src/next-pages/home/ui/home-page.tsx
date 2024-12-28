8699c4490e838abffba60be18b96436dc1807a07
Edanriell <Edanriell@gmail.com>
2024-12-28 14:51:14 +0200

    feature(projects): created markup and classnames

--- Client/src/next-pages/home/ui/home-page.tsx
+++ Client/src/next-pages/home/ui/home-page.tsx
@@ -2,12 +2,14 @@ import { type FC } from "react";
 
 import { Hero } from "./sections/hero/ui";
 import { Skills } from "./sections/skills/ui";
+import { Projects } from "./sections/projects/ui";
 
 export const HomePage: FC = () => {
 	return (
 		<>
 			<Hero />
 			<Skills />
+			<Projects />
 		</>
 	);
 };
--- Client/src/next-pages/home/ui/sections/projects/ui/projects.tsx
+++ Client/src/next-pages/home/ui/sections/projects/ui/projects.tsx
@@ -1,5 +1,118 @@
 import { type FC } from "react";
+import Image from "next/image";
+
+import designPortfolioProjectImage from "@public/images/raster/projects/design-portfolio.jpg";
+import eLearningLandingPageProjectImage from "@public/images/raster/projects/e-learning-landing-page.jpg";
+import todoWebAppProjectImage from "@public/images/raster/projects/todo-web-app.jpg";
+import entertainmentWebAppProjectImage from "@public/images/raster/projects/entertainment-web-app.jpg";
+import memoryGameProjectImage from "@public/images/raster/projects/memory-game.jpg";
+import artGalleryShowcaseProjectImage from "@public/images/raster/projects/art-gallery-showcase.jpg";
+
+import styles from "./projects.module.css";
 
 export const Projects: FC = () => {
-	return null;
+	return (
+		<section className={styles["section-projects"]}>
+			<header className={styles["section-projects__header"]}>
+				<h2 className={styles["section-projects__title"]}>Projects</h2>
+				<a className={styles["link"]} href="#">
+					Contact me
+				</a>
+			</header>
+			<ul
+				className={
+					styles["section-projects__projects-list"] + " " + styles["projects-list"]
+				}
+			>
+				<li className={styles["projects-list__project"]}>
+					<article className={styles["project"]}>
+						<Image
+							className={styles["project__image"]}
+							src={designPortfolioProjectImage}
+							alt="Design Portfolio project"
+						/>
+						<h3 className={styles["project__title"]}>Design Portfolio</h3>
+						<ul className={styles["project__tag-list"] + " " + styles["tag-list"]}>
+							<li className={styles["tag-list__tag"]}>Html</li>
+							<li className={styles["tag-list__tag"]}>Css</li>
+						</ul>
+					</article>
+				</li>
+				<li className={styles["projects-list__project"]}>
+					<article className={styles["project"]}>
+						<Image
+							className={styles["project__image"]}
+							src={eLearningLandingPageProjectImage}
+							alt="E-Learning Landing Page project"
+						/>
+						<h3 className={styles["project__title"]}>E-Learning Landing Page</h3>
+						<ul className={styles["project__tag-list"] + " " + styles["tag-list"]}>
+							<li className={styles["tag-list__tag"]}>Html</li>
+							<li className={styles["tag-list__tag"]}>Css</li>
+						</ul>
+					</article>
+				</li>
+				<li className={styles["projects-list__project"]}>
+					<article className={styles["project"]}>
+						<Image
+							className={styles["project__image"]}
+							src={todoWebAppProjectImage}
+							alt="Todo web app project"
+						/>
+						<h3 className={styles["project__title"]}>Todo web app</h3>
+						<ul className={styles["project__tag-list"] + " " + styles["tag-list"]}>
+							<li className={styles["tag-list__tag"]}>Html</li>
+							<li className={styles["tag-list__tag"]}>Css</li>
+							<li className={styles["tag-list__tag"]}>Javascript</li>
+						</ul>
+					</article>
+				</li>
+				<li className={styles["projects-list__project"]}>
+					<article className={styles["project"]}>
+						<Image
+							className={styles["project__image"]}
+							src={entertainmentWebAppProjectImage}
+							alt="Entertainment web app project"
+						/>
+						<h3 className={styles["project__title"]}>Entertainment web app</h3>
+						<ul className={styles["project__tag-list"] + " " + styles["tag-list"]}>
+							<li className={styles["tag-list__tag"]}>Html</li>
+							<li className={styles["tag-list__tag"]}>Css</li>
+							<li className={styles["tag-list__tag"]}>Javascript</li>
+						</ul>
+					</article>
+				</li>
+				<li className={styles["projects-list__project"]}>
+					<article className={styles["project"]}>
+						<Image
+							className={styles["project__image"]}
+							src={memoryGameProjectImage}
+							alt="Memory game project"
+						/>
+						<h3 className={styles["project__title"]}>Memory game</h3>
+						<ul className={styles["project__tag-list"] + " " + styles["tag-list"]}>
+							<li className={styles["tag-list__tag"]}>Html</li>
+							<li className={styles["tag-list__tag"]}>Css</li>
+							<li className={styles["tag-list__tag"]}>Javascript</li>
+						</ul>
+					</article>
+				</li>
+				<li className={styles["projects-list__project"]}>
+					<article className={styles["project"]}>
+						<Image
+							className={styles["project__image"]}
+							src={artGalleryShowcaseProjectImage}
+							alt="Art gallery showcase project"
+						/>
+						<h3 className={styles["project__title"]}>Art gallery showcase</h3>
+						<ul className={styles["project__tag-list"] + " " + styles["tag-list"]}>
+							<li className={styles["tag-list__tag"]}>Html</li>
+							<li className={styles["tag-list__tag"]}>Css</li>
+							<li className={styles["tag-list__tag"]}>Javascript</li>
+						</ul>
+					</article>
+				</li>
+			</ul>
+		</section>
+	);
 };
