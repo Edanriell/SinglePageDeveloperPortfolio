7d403d5aa3dd735e3bf9fc942b60abde7aa29b72
Edanriell <Edanriell@gmail.com>
2025-01-05 15:01:55 +0200

    feature(link-animations): created link component animations

--- Client/src/shared/ui/link/ui/link.tsx
+++ Client/src/shared/ui/link/ui/link.tsx
@@ -1,4 +1,7 @@
-import { type ComponentPropsWithoutRef, type FC, type ReactNode } from "react";
+"use client";
+
+import { type ComponentPropsWithoutRef, type FC, type ReactNode, useState } from "react";
+import { motion, type Variants } from "motion/react";
 
 import styles from "./link.module.css";
 
@@ -8,10 +11,91 @@ type LinkProps = {
 	children: ReactNode;
 } & ComponentPropsWithoutRef<"a">;
 
+type LinkContentParameters = {
+	className: string;
+	animationVariants: Variants;
+	children: ReactNode;
+};
+
+const linkAnimationVariants: Variants = {
+	initial: {
+		y: 0,
+		filter: "blur(0rem)"
+	},
+	active: {
+		y: "calc(-1 * var(--link-height))",
+		filter: "blur(5rem)"
+	},
+	inactive: {
+		y: 0,
+		filter: "blur(0rem)"
+	}
+};
+
+const shadowLinkAnimationVariants: Variants = {
+	initial: {
+		y: 0,
+		filter: "blur(5rem)"
+	},
+	active: {
+		y: "calc(-1 * var(--link-height))",
+		filter: "blur(0rem)"
+	},
+	inactive: {
+		y: 0,
+		filter: "blur(5rem)"
+	}
+};
+
 export const Link: FC<LinkProps> = ({ href, target = "_self", children, ...rest }) => {
-	return (
-		<a className={styles["link"]} href={href} target={target} {...rest}>
+	const [linkState, setLinkState] = useState<
+		"idle" | "hovered" | "unHovered" | "touched" | "unTouched"
+	>("idle");
+
+	const renderLinkContent = ({
+		className,
+		animationVariants,
+		children
+	}: LinkContentParameters) => (
+		<motion.span
+			className={className}
+			initial={"initial"}
+			animate={
+				linkState === "hovered" || linkState === "touched"
+					? "active"
+					: linkState === "unHovered" || linkState === "unTouched"
+						? "inactive"
+						: "initial"
+			}
+			variants={animationVariants}
+		>
 			{children}
+		</motion.span>
+	);
+
+	return (
+		<a
+			onMouseEnter={() => setLinkState("hovered")}
+			onMouseLeave={() => setLinkState("unHovered")}
+			onTouchStart={() => setLinkState("touched")}
+			onTouchEnd={() => setLinkState("unTouched")}
+			className={styles["link"]}
+			href={href}
+			target={target}
+			{...rest}
+		>
+			<div className={styles["link__content-wrapper"]}>
+				{renderLinkContent({
+					className: styles["link__text"] + " " + styles["link__text--color--white"],
+					animationVariants: linkAnimationVariants,
+					children
+				})}
+				{renderLinkContent({
+					className: styles["link__text"] + " " + styles["link__text--color--green"],
+					animationVariants: shadowLinkAnimationVariants,
+					children
+				})}
+			</div>
 		</a>
 	);
 };
