4a8a29e1f4094c894958b913fcbb495f0b301dce
Edanriell <Edanriell@gmail.com>
2025-01-07 00:17:53 +0200

    feature(project-card-animations): created styles

--- Client/src/entities/project/ui/project-card/ui/project-card.tsx
+++ Client/src/entities/project/ui/project-card/ui/project-card.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { type FC } from "react";
 import Image from "next/image";
 
@@ -13,24 +15,39 @@ type ProjectCardProps = Project;
 export const ProjectCard: FC<ProjectCardProps> = ({ name, image, tags, links }) => {
 	return (
 		<article className={styles["project"]}>
-			<Image
-				className={styles["project__image"]}
-				src={image}
-				alt={`${name} project`}
-				placeholder="blur"
-				width={540}
-				height={400}
-			/>
-			<h3 className={styles["project__title"]}>{name}</h3>
-			<Tags tags={tags} className={styles["project__tag-list"]} />
-			<div className={styles["project__links"]}>
-				<Link href={links.project} target="_blank">
-					View project
-				</Link>
-				<Link href={links.code} target="_blank">
-					View code
-				</Link>
+			<div className={styles["project__secondary-content"]}>
+				<Image
+					className={styles["project__image"]}
+					src={image}
+					alt={`${name} project`}
+					placeholder="blur"
+					width={540}
+					height={400}
+				/>
+				<div className={styles["project__links"]}>
+					<Link href={links.project} target="_blank">
+						View project
+					</Link>
+					<Link href={links.code} target="_blank">
+						View code
+					</Link>
+				</div>
+			</div>
+			<div className={styles["project__primary-content"]}>
+				<h3 className={styles["project__title"]}>{name}</h3>
+				<Tags tags={tags} className={styles["project__tag-list"]} />
+				<div className={styles["project__links"]}>
+					<Link href={links.project} target="_blank">
+						View project
+					</Link>
+					<Link href={links.code} target="_blank">
+						View code
+					</Link>
+				</div>
 			</div>
 		</article>
 	);
 };
+
+// CUSTOM HOOK FOR WIDTH DETERMINATION
+// CONDITIONALLUY RENDER LINKS BASED ON WIDTH
