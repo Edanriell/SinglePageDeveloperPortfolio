29e16f85c8ad25c8e31331999893c1122127400c
Edanriell <Edanriell@gmail.com>
2025-01-07 12:06:23 +0200

    feature(project-card-animations): created animations

--- Client/src/entities/project/ui/project-card/ui/project-card.tsx
+++ Client/src/entities/project/ui/project-card/ui/project-card.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { type FC } from "react";
+import { type FC, useState } from "react";
 import Image from "next/image";
+import { motion, type Variants } from "motion/react";
 
 import { Tags } from "@shared/ui/tags/ui";
 import { Link } from "@shared/ui/link/ui";
@@ -12,10 +13,68 @@ import styles from "./project-card.module.css";
 
 type ProjectCardProps = Project;
 
+const projectCardAnimationVariants: Variants = {
+	initial: {
+		opacity: 0
+	},
+	active: {
+		opacity: 1
+	},
+	inactive: {
+		opacity: 0
+	}
+};
+
+const projectLinkAnimationVariants: Variants = {
+	initial: {
+		opacity: 0,
+		x: -20,
+		filter: "blur(3rem)"
+	},
+	visible: {
+		opacity: 1,
+		x: 0,
+		filter: "blur(0rem)"
+	},
+	hidden: {
+		opacity: 0,
+		x: -20,
+		filter: "blur(3rem)"
+	}
+};
+
+const codeLinkAnimationVariants: Variants = {
+	initial: {
+		opacity: 0,
+		x: 20,
+		filter: "blur(3rem)"
+	},
+	visible: {
+		opacity: 1,
+		x: 0,
+		filter: "blur(0rem)"
+	},
+	hidden: {
+		opacity: 0,
+		x: 20,
+		filter: "blur(3rem)"
+	}
+};
+
 export const ProjectCard: FC<ProjectCardProps> = ({ name, image, tags, links }) => {
+	const [projectCardState, setProjectCardState] = useState<
+		"idle" | "hovered" | "unHovered" | "touched" | "unTouched"
+	>("idle");
+
 	return (
 		<article className={styles["project"]}>
-			<div className={styles["project__secondary-content"]}>
+			<div
+				onMouseEnter={() => setProjectCardState("hovered")}
+				onMouseLeave={() => setProjectCardState("unHovered")}
+				onTouchStart={() => setProjectCardState("touched")}
+				onTouchEnd={() => setProjectCardState("unTouched")}
+				className={styles["project__secondary-content"]}
+			>
 				<Image
 					className={styles["project__image"]}
 					src={image}
@@ -24,14 +83,48 @@ export const ProjectCard: FC<ProjectCardProps> = ({ name, image, tags, links }) 
 					width={540}
 					height={400}
 				/>
-				<div className={styles["project__links"]}>
-					<Link href={links.project} target="_blank">
-						View project
-					</Link>
-					<Link href={links.code} target="_blank">
-						View code
-					</Link>
-				</div>
+				<motion.div
+					animate={
+						projectCardState === "hovered" || projectCardState === "touched"
+							? "active"
+							: projectCardState === "unHovered" || projectCardState === "unTouched"
+								? "inactive"
+								: "initial"
+					}
+					variants={projectCardAnimationVariants}
+					className={styles["project__links"]}
+				>
+					<motion.div
+						variants={projectLinkAnimationVariants}
+						animate={
+							projectCardState === "hovered" || projectCardState === "touched"
+								? "visible"
+								: projectCardState === "unHovered" ||
+									  projectCardState === "unTouched"
+									? "hidden"
+									: "initial"
+						}
+					>
+						<Link href={links.project} target="_blank">
+							View project
+						</Link>
+					</motion.div>
+					<motion.div
+						variants={codeLinkAnimationVariants}
+						animate={
+							projectCardState === "hovered" || projectCardState === "touched"
+								? "visible"
+								: projectCardState === "unHovered" ||
+									  projectCardState === "unTouched"
+									? "hidden"
+									: "initial"
+						}
+					>
+						<Link href={links.code} target="_blank">
+							View code
+						</Link>
+					</motion.div>
+				</motion.div>
 			</div>
 			<div className={styles["project__primary-content"]}>
 				<h3 className={styles["project__title"]}>{name}</h3>
